2a7235331d289c984d51a4a85333315bbab8d7d0
KevinHackNinja <61767060+KevinHackNinja@users.noreply.github.com>
2021-03-27 10:39:12 -0600

    Add files via upload
    Se añadio nuevo requerimiento.
    Chat:
    Mensaje inicia con # se manda de atrás para adelante (#hello world) --> dlrow olleh
    Mensaje finaliza con @ se manda los caracteres intercalados (hello world@) --> hlowrd

---
+++ Cliente_Chat/Cliente_Chat/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace Cliente_Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtmjs = new System.Windows.Forms.TextBox();
+            this.btnenviarmsj = new System.Windows.Forms.Button();
+            this.btnconectar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(476, 184);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(232, 20);
+            this.txtUser.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(559, 156);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Usuario";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(382, 394);
+            this.listBox1.TabIndex = 2;
+            // 
+            // txtmjs
+            // 
+            this.txtmjs.Location = new System.Drawing.Point(422, 276);
+            this.txtmjs.Name = "txtmjs";
+            this.txtmjs.Size = new System.Drawing.Size(331, 20);
+            this.txtmjs.TabIndex = 3;
+            // 
+            // btnenviarmsj
+            // 
+            this.btnenviarmsj.Location = new System.Drawing.Point(532, 302);
+            this.btnenviarmsj.Name = "btnenviarmsj";
+            this.btnenviarmsj.Size = new System.Drawing.Size(100, 27);
+            this.btnenviarmsj.TabIndex = 4;
+            this.btnenviarmsj.Text = "Enviar";
+            this.btnenviarmsj.UseVisualStyleBackColor = true;
+            this.btnenviarmsj.Click += new System.EventHandler(this.btnenviarmsj_Click);
+            // 
+            // btnconectar
+            // 
+            this.btnconectar.Location = new System.Drawing.Point(532, 210);
+            this.btnconectar.Name = "btnconectar";
+            this.btnconectar.Size = new System.Drawing.Size(100, 29);
+            this.btnconectar.TabIndex = 5;
+            this.btnconectar.Text = "Conectar";
+            this.btnconectar.UseVisualStyleBackColor = true;
+            this.btnconectar.Click += new System.EventHandler(this.btnconectar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(511, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(197, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Chat Multiusuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(568, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Mensaje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnconectar);
+            this.Controls.Add(this.btnenviarmsj);
+            this.Controls.Add(this.txtmjs);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtmjs;
+        private System.Windows.Forms.Button btnenviarmsj;
+        private System.Windows.Forms.Button btnconectar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Cliente_Chat/Cliente_Chat/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Net.Sockets;
+using System.IO;
+using Transitions;
+namespace Cliente_Chat
+{
+    public partial class Form1 : Form
+    {
+        static private NetworkStream str;
+        static private StreamWriter strw;
+        static private StreamReader strr;
+        static private TcpClient client = new TcpClient();
+        static private string user = "unknow";
+        
+        private static object cadena;
+
+        private delegate void PadreItem(String s);
+
+
+
+        //metodo para agregar texto en el listbox
+        private void AddText(String s)
+        {
+            listBox1.Items.Add(s);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        // Metodo para imprimir en el chat 
+        void imprimir()
+        {
+            //mientras el cliente tenga conexión aplicara lo que este dentro 
+            // de la excepción
+
+            while (client.Connected)
+            {
+                try
+                {
+                    this.Invoke(new PadreItem(AddText), strr.ReadLine());
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("No se ha podido conectar...");
+                    Application.Exit();
+                }
+            }
+
+
+        }
+
+        //metódo conectar el cual nos conectará a el servidor con la 
+        // siguiente ip y con su puerto
+        void Conectar()
+        {
+            try
+            {
+                client.Connect("127.0.0.1", 8000);
+                if (client.Connected)
+                {
+                    Thread t = new Thread(imprimir);
+                    str = client.GetStream();
+                    strw = new StreamWriter(str);
+                    strr = new StreamReader(str);
+
+                    strw.WriteLine(user);
+                    strw.Flush();
+
+                    t.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Servidor no disponible");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Por favor verifique que el servidor este en linea");
+                Application.Exit();
+            }
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnconectar_Click(object sender, EventArgs e)
+        {
+            user = txtUser.Text;
+            Conectar();
+
+            Transition tr = new Transition(new TransitionType_EaseInEaseOut(900));
+            tr.add(label1, "Left", 555);
+            tr.add(txtUser, "Left", 555);
+            tr.add(btnconectar, "Left", 555);
+            tr.run();
+
+            txtUser.Enabled = false;
+            btnconectar.Enabled = false;
+
+        }
+
+        private void btnenviarmsj_Click(object sender, EventArgs e)
+        {
+
+
+            // Mensaje finaliza con @ se manda los caracteres intercalados (hello world@) --> hlowrd
+            string msj =txtmjs.Text;
+
+            if (msj.EndsWith("@"))
+            {
+                string it = msj.Substring(3, 6);
+                label2.Text = it;
+                strw.WriteLine(msj);
+                strw.Flush();
+                txtmjs.Clear();
+            }
+            else
+            {
+                //Mensaje inicia con # se manda de atrás para adelante (#hello world) --> dlrow olleh
+                if (msj.StartsWith("#"))
+                {
+                    string rv = ReverseString(msj);
+                    label1.Text = rv;
+                    strw.WriteLine(msj);
+                    strw.Flush();
+                    txtmjs.Clear();
+                }
+
+            }
+
+
+        }
+
+        static string ReverseString(string s)
+        {
+
+            char[] array = s.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+    }
+}
